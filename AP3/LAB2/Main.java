bff4630b9ccbba27cc00bf088059aba4f6943d73
Rishabh Jain <2r10j5@gmail.com>
2026-01-22 10:11:33 +0530

    Add files via upload

---
+++ AP3/LAB2/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Mohen.setAge(90);
+	    Mohen.setAge();
+	}
+}
---
+++ AP3/LAB2/Mohen.java
@@ -0,0 +1,11 @@
+class Mohen{
+    private static int age;
+    public static void setAge(int y)
+    {
+        age=y;
+    }
+    public static void setAge()
+    {
+    System.out.println(age);
+    }
+}
