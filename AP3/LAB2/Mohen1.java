acf9a9a900b691ffce75272b244e6591a8dff165
Rishabh Jain <2r10j5@gmail.com>
2026-01-22 10:21:56 +0530

    Delete AP3/LAB2 directory

--- AP3/LAB2/Mohen1.java
+++
@@ -1,8 +0,0 @@
-class Mohen1{
-    private static int age;
-    public static void setAge(int age){
-        this.age=age;
-    }
-    public static void setAge(){
-    System.out.println(this.age);}
-}
